a301734c0a2940b11ac0b0f44a14e59519224494
cciric <cciric@nexient.com>
2020-10-20 23:04:31 -0400

    successful GET employees request using external json file for that has an employee

--- routes/main.go
+++ routes/main.go
@@ -5,39 +5,51 @@ import (
 "github.com/gorilla/mux"
 "encoding/json"
 "net/http"
+"io/ioutil"
 
 )
 
-//structs
+type Project struct{
+Id string `json:"id"`
+Name string `json`
+}
+
 type Employee struct{
 
-Id string `json:id`
-FirstName string `json:first`
-LastName string `json:first`
+Id string `json:"id"`
+FirstName string `json:"first"`
+LastName string `json:"last"`
 
 }
 
 var newEmployee Employee
 
-var employees []Employee
-
 //main
 func main() {
 
+content, err := ioutil.ReadFile("employees.json")
+if err != nil {
+fmt.Println("could not read the file")
+}
+
+err2 := json.Unmarshal(content, &newEmployee)
+if err2 != nil {
+fmt.Println("unmarshal error")
+}
+
 r:=mux.NewRouter()
 
-newEmployee = Employee{
-            Id:"1",
-            FirstName:"Chris",
-            LastName:"Ciric"}
+// newEmployee = Employee{
+//             Id:"1",
+//             FirstName:"Chris",
+//             LastName:"Ciric"}
 
-employees = append(employees, newEmployee)
-fmt.Println(newEmployee)
 
 r.HandleFunc("/employees", getEmployees).Methods("GET")
 // r.HandleFunc("/project", getProjects).Methods("GET")
 // r.HandleFunc("/sow", getSow).Methods("GET")
 // r.HandleFunc("/clients", getClients).Methods("GET")
+
 
 http.ListenAndServe(":8081", r)
 
@@ -45,10 +57,9 @@ http.ListenAndServe(":8081", r)
 
 func getEmployees(w http.ResponseWriter, r *http.Request){
 w.Header().Set("Content-Type", "application/json")
-fmt.Println("new test")
-fmt.Println(newEmployee)
 json.NewEncoder(w).Encode(newEmployee)
 }
+
 
 
 
