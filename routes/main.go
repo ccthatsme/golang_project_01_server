34a511fba474624123d4c62f05b5c93366f4d86c
cciric <cciric@nexient.com>
2020-10-21 12:52:18 -0400

    formatted code using gofmt -w main.go command

--- routes/main.go
+++ routes/main.go
@@ -1,42 +1,33 @@
 package main
 
 import (
-"fmt"
-"github.com/gorilla/mux"
-"encoding/json"
-"net/http"
-"io/ioutil"
-
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
 )
 
 type Client struct {
-
-Id string `json:"id"`
-ClientName string `json:"clientName"`
-ContractLength int `json:"contractLength"`
-
+	Id             string `json:"id"`
+	ClientName     string `json:"clientName"`
+	ContractLength int    `json:"contractLength"`
 }
 
-type Project struct{
-
-Id string `json:"id"`
-ProjectName string `json:"projectName"`
-
+type Project struct {
+	Id          string `json:"id"`
+	ProjectName string `json:"projectName"`
 }
 
-type SOW struct{
-
-Id string `json:"id"`
-WorkName string `json:"workName"`
-
+type SOW struct {
+	Id       string `json:"id"`
+	WorkName string `json:"workName"`
 }
 
-type Employee struct{
-
-Id string `json:"id"`
-FirstName string `json:"first"`
-LastName string `json:"last"`
-
+type Employee struct {
+	Id        string `json:"id"`
+	FirstName string `json:"first"`
+	LastName  string `json:"last"`
 }
 
 var newEmployee Employee
@@ -46,79 +37,73 @@ var newClient Client
 
 func main() {
 
-empContent, empErr := ioutil.ReadFile("employees.json")
-if empErr != nil {
-fmt.Println("could not read the file")
-}
+	empContent, empErr := ioutil.ReadFile("employees.json")
+	if empErr != nil {
+		fmt.Println("could not read the file")
+	}
 
-empErr2 := json.Unmarshal(empContent, &newEmployee)
-if empErr2 != nil {
-fmt.Println("unmarshal error")
-}
+	empErr2 := json.Unmarshal(empContent, &newEmployee)
+	if empErr2 != nil {
+		fmt.Println("unmarshal error")
+	}
 
-projectContent, proErr := ioutil.ReadFile("projects.json")
-if proErr != nil {
-fmt.Println("could not read the file")
-}
+	projectContent, proErr := ioutil.ReadFile("projects.json")
+	if proErr != nil {
+		fmt.Println("could not read the file")
+	}
 
-proErr2 := json.Unmarshal(projectContent, &newProject)
-if proErr2 != nil {
-fmt.Println("unmarshal error")
-}
+	proErr2 := json.Unmarshal(projectContent, &newProject)
+	if proErr2 != nil {
+		fmt.Println("unmarshal error")
+	}
 
-sowContent, sowErr := ioutil.ReadFile("sow.json")
-if sowErr != nil {
-fmt.Println("could not read the file")
-}
+	sowContent, sowErr := ioutil.ReadFile("sow.json")
+	if sowErr != nil {
+		fmt.Println("could not read the file")
+	}
 
-sowErr2 := json.Unmarshal(sowContent, &newSow)
-if sowErr2 != nil {
-fmt.Println("unmarshal error")
-}
+	sowErr2 := json.Unmarshal(sowContent, &newSow)
+	if sowErr2 != nil {
+		fmt.Println("unmarshal error")
+	}
 
-clientContent, clientErr := ioutil.ReadFile("clients.json")
-if clientErr != nil {
-fmt.Println("could not read the file")
-}
+	clientContent, clientErr := ioutil.ReadFile("clients.json")
+	if clientErr != nil {
+		fmt.Println("could not read the file")
+	}
 
-clientErr2 := json.Unmarshal(clientContent, &newClient)
-if clientErr2 != nil {
-fmt.Println("unmarshal error")
-}
+	clientErr2 := json.Unmarshal(clientContent, &newClient)
+	if clientErr2 != nil {
+		fmt.Println("unmarshal error")
+	}
 
-r:=mux.NewRouter()
+	r := mux.NewRouter()
 
+	r.HandleFunc("/employees", getEmployees).Methods("GET")
+	r.HandleFunc("/projects", getProjects).Methods("GET")
+	r.HandleFunc("/sow", getSow).Methods("GET")
+	r.HandleFunc("/clients", getClients).Methods("GET")
 
-r.HandleFunc("/employees", getEmployees).Methods("GET")
-r.HandleFunc("/projects", getProjects).Methods("GET")
-r.HandleFunc("/sow", getSow).Methods("GET")
-r.HandleFunc("/clients", getClients).Methods("GET")
-
-
-http.ListenAndServe(":8081", r)
+	http.ListenAndServe(":8081", r)
 
 }
 
-func getEmployees(w http.ResponseWriter, r *http.Request){
-w.Header().Set("Content-Type", "application/json")
-json.NewEncoder(w).Encode(newEmployee)
+func getEmployees(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newEmployee)
 }
 
-func getProjects(w http.ResponseWriter, r *http.Request){
-w.Header().Set("Content-Type", "application/json")
-json.NewEncoder(w).Encode(newProject)
+func getProjects(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newProject)
 }
 
-func getSow(w http.ResponseWriter, r *http.Request){
-w.Header().Set("Content-Type", "application/json")
-json.NewEncoder(w).Encode(newSow)
+func getSow(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newSow)
 }
 
-func getClients(w http.ResponseWriter, r *http.Request){
-w.Header().Set("Content-Type", "application/json")
-json.NewEncoder(w).Encode(newClient)
+func getClients(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newClient)
 }
-
-
-
-
