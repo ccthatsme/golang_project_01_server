fe2105a6ddc1432792220d5ee77e64251212c776
cciric <cciric@nexient.com>
2020-10-22 22:57:40 -0400

    got a succesfull authentication response in postman, need to adjust response body

--- datasources/nexient-authentication.go
+++ datasources/nexient-authentication.go
@@ -25,16 +25,36 @@ var user Credential
 
 func Authenticate() ([]byte){
 
-user = Credential{Username:"cciric",Password:"Dustbumper6025!"}
+user = Credential{Username:"cciric",Password:"Dustbumper6025!\""}
 json, _ := json.Marshal(user)
 
-resp, err := http.Post("https://portal.nexient.com/gateway/api/authentication/authenticate", "application/json", bytes.NewBuffer(json))
+client := http.Client{
+
+}
+
+request, err := http.NewRequest("POST", "https://portal.nexient.com/gateway/api/authentication/authenticate", bytes.NewBuffer(json))
+request.Header.Set("Content-type", "application/json")
+request.Header.Set("Version", "2")
+
+if err != nil {
+fmt.Println("line 40 nex-auth")
+}
+
+resp, err := client.Do(request)
 if err != nil{
 fmt.Println("error making post request, nex-auth.go")
 }
+
+// resp, err := http.Post("https://portal.nexient.com/gateway/api/authentication/authenticate", "application/json", bytes.NewBuffer(json))
+// if err != nil{
+// fmt.Println("error making post request, nex-auth.go")
+// }
+
 defer resp.Body.Close()
 
  bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+fmt.Println(string(bodyBytes))
 
 return bodyBytes
 }
